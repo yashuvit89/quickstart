efa1573fc6cdc3932c36218200ac8c241ff2632b
Yaswanth <yaswanth@Yaswanth.local>
2017-02-17 11:07:26 +0530

    dashboard module

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,7 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+
+
+import { Location } from '@angular/common';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
   template: `
     <div *ngIf="hero">
@@ -11,12 +18,29 @@ import { Hero } from './hero';
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
+      <button (click)="goBack()">Back</button>
     </div>
   `
 
 })
 
-export class HeroDetailComponent{
-  @Input()
-  hero: Hero;
+export class HeroDetailComponent implements OnInit{
+  @Input() hero: Hero;
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  goBack(): void {
+    this.location.back();
+  }
+
 }
